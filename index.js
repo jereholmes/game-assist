f0e45a668bc083d2b3781e4305f837c7d43ff844
jereholmes <holmesjeremiah756@gmail.com>
2022-02-23 00:29:06 +0000

    added getDistance

--- index.js
+++ index.js
@@ -61,6 +61,13 @@
           }
         };
       },
+      getDistance(pointA, pointB){
+        const 
+        distanceX = pointB.x - pointA.x,
+        distanceY = pointB.y - pointA.y,
+        distance = math.sqrt(distanceX * distanceX + distanceY * distanceY);
+        return distance;
+      },
     },
   };
 }(window, window._));
